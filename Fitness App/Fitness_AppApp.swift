870fe5ccc1f9b04e437f4996608a63b4051200c7
Fabricio <calebrodriguez003@icloud.com>
2023-12-26 13:56:46 -0500

    Initial Commit

---
+++ Fitness App/Fitness_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_AppApp.swift
+//  Fitness App
+//
+//  Created by Fabricio Rodriguez on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
